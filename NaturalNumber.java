22ec4f1615d324bc80935ffb3556a4238bbdcaa8
Santosh Kumar Dhawal <162580448+GitH05@users.noreply.github.com>
2024-09-28 23:57:33 +0530

    natural Number;;

---
+++ NaturalNumber.java
@@ -0,0 +1,30 @@
+/*
+PRINCIPLE
+-main function
+-base condition
+-task || work to do
+*/
+
+//sum of n NaturalNumber
+public class NaturalNumber
+{
+
+//sum function:
+public static void printSum(int i, int n, int sum)
+{
+if(i==n)
+{
+sum += i;
+System.out.println(sum);
+return;
+}
+sum += i;
+printSum(i+1, n, sum);
+}
+
+
+public static void main(String[] args)
+{
+printSum(1, 5, 0);
+}
+}
