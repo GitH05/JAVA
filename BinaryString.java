80e3c2af411ed46ce06fc0c3bc395aa1d2f0c15d
Santosh Kumar Dhawal <162580448+GitH05@users.noreply.github.com>
2024-10-12 23:09:04 +0530

    Convert a binary string["1111"] into an integer[15].

---
+++ BinaryString.java
@@ -0,0 +1,9 @@
+public class BinaryString {
+    public static void main(String[] args) {
+        // String binaryNumber = "1111";
+        int binaryNumber = 1111;
+        String number = Integer.toBinaryString(binaryNumber);
+        System.out.println(number);
+    }
+    
+}
