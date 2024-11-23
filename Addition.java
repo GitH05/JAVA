c8935066b8b4d7062d76647f58e1c6841b4f757c
Santosh Kumar Dhawal <162580448+GitH05@users.noreply.github.com>
2024-11-24 00:02:30 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package Function_Method;
+
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println(Sum(1,2));
+    }
+    static int Sum(int a, int b) {
+        return a+b;
+    }   
+}
