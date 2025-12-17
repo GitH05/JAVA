ee85aa2de95ed4cba512266098cb8d77c50ab4dc
GitH05 <santoshdhawal05@gmail.com>
2025-12-18 00:56:18 +0530

    refactored and a new way explained

--- ReverseString.java
+++ ReverseString.java
@@ -6,8 +6,12 @@ public class ReverseString {
         System.out.println("Enter character");
         String str = s.nextLine();
         char[] reverse = str.toCharArray();
+        // reverse using for loop
         for (int i = reverse.length - 1; i >= 0; i--) {
             System.out.print(reverse[i]);
         }
+        // reverse using StringBuffer
+        StringBuffer rev = new StringBuffer(str);
+        System.out.println("\nReverse: " + rev.reverse());
     }
 }
