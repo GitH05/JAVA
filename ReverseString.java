ac3f13dbb0174f5dec892641a701936945ebf898
GitH05 <santoshdhawal05@gmail.com>
2025-12-18 00:48:47 +0530

    refactored

--- ReverseString.java
+++ ReverseString.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
-public class ReverseString
-{
-public static void main(String[] args)
-{
-Scanner s=new Scanner(System.in);
-System.out.println("Enter character");
-String str=s.nextLine();
-char[] reverse=str.toCharArray();
-for(int i=reverse.length-1;i>=0;i--)
-{
-System.out.print(reverse[i]);
-}
-}
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter character");
+        String str = s.nextLine();
+        char[] reverse = str.toCharArray();
+        for (int i = reverse.length - 1; i >= 0; i--) {
+            System.out.print(reverse[i]);
+        }
+    }
 }
