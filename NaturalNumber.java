a28d9a875e5a43f007a9f67470b3e883f47048ff
Santosh Kumar Dhawal <162580448+GitH05@users.noreply.github.com>
2026-08-09 15:54:28 +0530

    Update NaturalNumber.java

--- NaturalNumber.java
+++ NaturalNumber.java
@@ -8,20 +8,18 @@ PRINCIPLE
 //sum of n NaturalNumber
 public class NaturalNumber
 {
-
 //sum function:
 public static void printSum(int i, int n, int sum)
-{
-if(i==n)
-{
-sum += i;
-System.out.println(sum);
-return;
-}
+  {
+  if(i==n)
+  {
+  sum += i;
+  System.out.println(sum);
+  return;
+  }
 sum += i;
 printSum(i+1, n, sum);
 }
-
 
 public static void main(String[] args)
 {
