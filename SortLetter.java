62e49cf33488543248857bd67ccdcc6377fb8010
GitH05 <santoshdhawal05@gmail.com>
2026-08-10 08:01:32 +0530

    formatted

--- SortLetter.java
+++ SortLetter.java
@@ -1,20 +1,19 @@
 public class SortLetter {
-    
+
     public static void main(String[] args) {
         String word = "hello hi";
         // String[] words = word.split(" ");
         char[] chars = word.toCharArray();
 
-        for(int i=0; i<chars.length;i++) {
-            for(int j=i+1; j<chars.length;j++)
-            {
-                if(chars[i]>chars[j]) {
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] > chars[j]) {
                     char temp = chars[i];
                     chars[i] = chars[j];
                     chars[j] = temp;
                 }
             }
         }
-                    System.out.println(chars);
+        System.out.println(chars);
     }
 }
