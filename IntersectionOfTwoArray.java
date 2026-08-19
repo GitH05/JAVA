3377de00bd5a7a995abfef7752f7f01c0cfc1166
GitH05 <santoshdhawal05@gmail.com>
2026-08-19 09:34:00 +0530

    method 2nd used to avoid the duplicate elements

--- IntersectionOfTwoArray.java
+++ IntersectionOfTwoArray.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class IntersectionOfTwoArray {
     public static void main(String[] args) {
         IntersectionOfTwoArray intersection = new IntersectionOfTwoArray();
@@ -6,9 +10,10 @@ public class IntersectionOfTwoArray {
         int[] arr2 = { 4, 5, 6, 7, 8 };
 
         intersection.findIntersection(arr1, arr2);
+        intersection.IntersectionWithoutDuplicate();
     }
 
-    // method to find the intersection of two arrays - 1
+    // method to find the intersection of two arrays - 1 | duplicate may exist
     public void findIntersection(int[] arr1, int[] arr2) {
         System.out.print("Intersection: ");
         for (int i = 0; i < arr1.length; i++) {
@@ -20,17 +25,31 @@ public class IntersectionOfTwoArray {
         }
     }
     /*
-     if there multiple number of same type then it will print all of them and
-     their would be duplicate like 
-     
-     int[] arr1 = {1, 2, 2, 3};
-     int[] arr2 = {2, 2, 4};
-     
-     code would print 2 2 2 2, because each 2 in arr1 matches each 2 in arr2.
+     * if there multiple number of same type then it will print all of them and
+     * their would be duplicate like
+     * 
+     * int[] arr1 = {1, 2, 2, 3};
+     * int[] arr2 = {2, 2, 4};
+     * 
+     * code would print 2 2 2 2, because each 2 in arr1 matches each 2 in arr2.
      */
 
+    // Here method to find the intersection of two arrays - 2 | avoid duplicates
+    public void IntersectionWithoutDuplicate() {
+        int[] arr1 = { 1, 2, 3, 3, 5, 7, 7 };
+        int[] arr2 = { 4, 2, 3, 3, 6, 7, 5 };
 
+        Set<Integer> set1 = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set1.add(arr1[i]);
+        }
 
-     // Here method to find the intersection of two arrays - 2 | avoid duplicates
-
+        Set<Integer> set2 = new LinkedHashSet<>(); // maintains insertion order
+        for (int num : arr2) {
+            if (set1.contains(num)) {
+                set2.add(num);
+            }
+        }
+        System.out.println("\nIntersection without duplicates: " + set2);
+    }
 }
