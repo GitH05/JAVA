c4ac4119a6a23710e61751f92958c0cff63d7455
GitH05 <santoshdhawal05@gmail.com>
2026-08-19 08:38:50 +0530

    Find the Intersection of Two Arrays

---
+++ IntersectionOfTwoArray.java
@@ -0,0 +1,20 @@
+public class IntersectionOfTwoArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {4, 5, 6, 7, 8};
+        
+        findIntersection(arr1, arr2);
+    }
+
+    // method to find the intersection of two arrays - 1
+    public static void findIntersection(int[] arr1, int[] arr2) {
+        System.out.print("Intersection: ");
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");
+                }
+            }
+        }
+    }
+    }
