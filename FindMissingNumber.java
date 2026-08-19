ef83933a39abfa0b4851bed7c566b6a3869b776b
GitH05 <santoshdhawal05@gmail.com>
2026-08-19 16:41:39 +0530

    Find Missing Number via XOR property | easy and quick style

---
+++ FindMissingNumber.java
@@ -0,0 +1,21 @@
+/* 
+    Time complexity: O(n) [ loop runs n times ]
+    Space complexity: O(1)  constant variable used
+*/
+
+public class FindMissingNumber {
+    public static void main(String args[]) {
+        int[] arr = { 0, 3, 1 };
+        int n = arr.length;
+        int result = 0;
+
+        for (int i = 0; i <= n; i++) {
+            result = result ^ i; // 0 ^ 1 ^ 2 ^ 3
+        }
+
+        for (int i : arr) {
+            result = result ^ i; // 0 ^ 1 ^ 2 ^ 3 ^ 0 ^ 3 ^ 1 -> 2 remains via XOR property
+        }
+        System.out.println("The missing number is: " + result);
+    }
+}
