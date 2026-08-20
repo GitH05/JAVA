8f97d83cfe22172fee2a1e1f555ff770a9a64a4c
GitH05 <santoshdhawal05@gmail.com>
2026-08-20 13:11:49 +0530

    checked

--- Lists.java
+++ Lists.java
@@ -1,15 +1,14 @@
+
 //list
 import java.util.*;
-public class Lists
-{
-public static void main(String[] args)
-{
-	System.out.println("List");
-	List<Integer> arr = new ArrayList<>();
-	for(int i =0;i<=5;i++)
-	{
-		arr.add((int) (i*2));
+
+public class Lists {
+	public static void main(String[] args) {
+		System.out.println("List");
+		List<Integer> arr = new ArrayList<>();
+		for (int i = 0; i <= 5; i++) {
+			arr.add((int) (i * 2));
+		}
+		System.out.println(arr);
 	}
-	System.out.println(arr);
-}
 }
