a319dde792be4928afbe30f7b141d1e26e3c9421
GitH05 <santoshdhawal05@gmail.com>
2026-08-20 12:34:28 +0530

    formatted

--- FindMissingNumber.java
+++ FindMissingNumber.java
@@ -12,7 +12,6 @@ public class FindMissingNumber {
         for (int i = 0; i <= n; i++) {
             result = result ^ i; // 0 ^ 1 ^ 2 ^ 3
         }
-
         for (int i : arr) {
             result = result ^ i; // 0 ^ 1 ^ 2 ^ 3 ^ 0 ^ 3 ^ 1 -> 2 remains via XOR property
         }
